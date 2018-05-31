3b1998df80af0ac471e7f52517e75dcd4d7919d6
homegirl-fxd <39789543+homegirl-fxd@users.noreply.github.com>
2018-05-31 15:37:57 +0800

    Add files via upload

---
+++ ProjectMain.java
@@ -0,0 +1,14 @@
+package myoneproject;
+
+public class ProjectMain {
+
+	public ProjectMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
